6f5b58385538c0f5c929b7e25bb98d904655b93e
PAiratEe <airatsafin65578@gmail.com>
2022-04-01 11:18:00 +0300

    v4

--- approximate_calculation.cpp
+++ approximate_calculation.cpp
@@ -2,11 +2,17 @@
 
 double func(double x) {
     //return sin(x) + exp(3 * x) - x; // легко берущийся интеграл
+    //return - 2.434;
+    //return 2 * x;
+    //return 1.432 * x * x;
     return -4 * x * x * x + 1.432 * x * x + 2 * x - 2.434;
 }
 
 double integral(double a, double b) {
     //return -cos(b) - b * b / 2 + exp(3 * b) / 3 + cos(a) + a * a / 2 - exp(3 * a) / 3; // точное значение интеграла
+    //return - 2.434 * b + 2.434 * a;
+    //return b * b - a * a;
+    //return 179 * b * b * b / 375 - 179 * a * a * a / 375;
     return - b * b * b * b + 179 * b * b * b / 375 + b * b - 2.434 * b + a * a * a * a - 179 * a * a * a / 375 - a * a + 2.434 * a;
 }
 
